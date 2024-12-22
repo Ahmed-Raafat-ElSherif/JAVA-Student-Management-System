6d95ad5d96f4b1016053ead54bc7c18e36cb057e
Ahmed-M-Ezzat <Ahmed_M_Ezzat@outlook.com>
2024-12-22 23:55:08 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     // Main class serves as the entry point of the application.
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // Create an instance of LoginGUI and set it to visible, launching the login interface.
         new LoginGUI().setVisible(true);
     }
